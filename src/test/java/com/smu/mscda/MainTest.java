97c97fb87804b52132e407ca2abf49e1721fee34
Kedar-Gaikwad <kedar.gaikwad.kg@gmail.com>
2025-03-18 23:13:40 -0300

    MavenAssignment

---
+++ src/test/java/com/smu/mscda/MainTest.java
@@ -0,0 +1,23 @@
+package com.smu.mscda;
+
+import static org.junit.Assert.assertEquals;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testCapitalizeString() {
+        String input = "hello";
+        String expected = "Hello";
+        assertEquals(expected, StringUtils.capitalize(input));
+    }
+
+    @Test
+    public void testMd5Hash() {
+        String input = "Hello";
+        String expectedMd5 = DigestUtils.md5Hex(input);
+        assertEquals(expectedMd5, DigestUtils.md5Hex(input));
+    }
+}
